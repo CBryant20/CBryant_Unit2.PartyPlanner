914d4096fe003c17b09cbd94d8e6f133c14be3ce
Charles Bryant <cwbryant25@yahoo.com>
2024-02-06 12:45:16 -0800

    CSS file added

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const endpoint = "/events";
  *
  */
 
+//////////// STATE ////////////////////
 /**
  * @typedef {State}
  */
@@ -35,5 +36,18 @@ const state = {
 - Each party will have a delete button
   - Delete will be selected to delete that particular event
 
-
+1.  Which components can be created directly in the HTML? Which components need to be created in JavaScript?
+2. Can you render mock data to the page?
+3. Can you render real data to the page?
+4. Are you able to fetch an array of all the parties from the API?
+5. Is state correctly updated to match the data from the API?
+6. Are you passing the correct arguments to fetch?
+7. Does the API return an error? If so, what is the error message?
+8. Is there an event listener on the form? Does it correctly add a new party to the list of parties?
+9. Is there an event listener attached to each delete button? Does it correctly remove a party from the list of parties?
 */
+
+// Update state with an array of recipe objects from the API via the internet
+////////////// RENDER /////////////////////
+
+/////////////// SCRIPT /////////////////////
